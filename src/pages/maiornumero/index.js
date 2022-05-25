934957ddfd7a9865ad394283eb73148174e83785
Gabriel de Araújo Silva <gabriel0764@hotmail.com>
2022-05-25 12:54:24 -0300

    ADICIONADA MAIS DUAS INTEGRAÇÕES ( FREQUÊNCIA E MAIOR NUMERO ) --JAQUELLINE

--- src/pages/maiornumero/index.js
+++ src/pages/maiornumero/index.js
@@ -1,9 +1,39 @@
-
+import { useState } from "react"
+import axios from "axios";
 
 export default function Index() {
+    const [numeros, setNumeros] = useState([]);
+    const [numero, setNumero] = useState(0);
+    const [resposta, setResposta] = useState('');
+
+
+    function adicionar() {
+       setNumeros([...numeros, numero])
+       setNumero('');
+    }
+
+    async function verificarMaior() {
+       const resp = await axios.post('http://localhost:5000/dia2/maiorNumero', numeros);
+       setResposta(resp.data.MaiorNumero);
+    }
+
     return (
         <main>
             <h1> Maior Número </h1>
+
+            <div> 
+                Número: <input type='text' value={numero} onChange={e => setNumero(Number(e.target.value))} /> <button onClick={adicionar} > Adicionar número </button>
+            </div>
+
+            <div>
+                {numeros.map(item =>
+                  <div> {item} </div>
+                )}
+            </div>
+
+            <div>
+                <button onClick={verificarMaior}> Verificar</button> Maior número é {resposta}
+            </div>
         </main>
     )
 }
